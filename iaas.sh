28ce366b658f48bce4a9b3f0dfda615976657d9d
nagarajui7 <nagaraju.batchu1@gmail.com>
2019-04-04 00:21:15 +0530

    Update iaas.sh

--- iaas.sh
+++ iaas.sh
@@ -1,4 +1,8 @@
 num=$1
+#gcloud authentication
+gcloud auth activate-service-account --key-file /home/reddisekhara_n/name.json 
+export GOOGLE_CLOUD_KEYFILE_JSON=/home/reddisekhara_n/name.json
+
 #number of vm's to be created
 sed -i "s/count =.*/count = $num/g" main.tf
 
@@ -9,4 +13,3 @@ terraform apply out.terraform
 
 #list of vm's created
 gcloud compute instances list | awk '{print $5}'
-
