02c631fff5fd225618368a895143185c60063837
nagarajui7 <nagaraju.batchu1@gmail.com>
2019-04-08 15:16:57 +0530

    Update iaas.sh

--- iaas.sh
+++ iaas.sh
@@ -1,21 +1,21 @@
 num=$1
-#gcloud authentication
+echo '#####################gcloud authentication#####################'
 gcloud auth activate-service-account --key-file /home/reddisekhara_n/iaas-provision-236417-83c0879798aa.json
 export GOOGLE_CLOUD_KEYFILE_JSON=/home/reddisekhara_n/iaas-provision-236417-83c0879798aa.json
 
-#number of vm's to be created
+echo '######################number of vm's to be created#####################'
 sed -i "s/count =.*/count = $num/g" main.tf
 
-#vm creation
+echo '######################vm creation#####################'
 terraform init
 terraform plan -out out.terraform
 terraform apply out.terraform
 
-#list of vm's created
+echo '######################list of vm's created#####################'
 gcloud compute instances list | awk '{print $5}' | sed 1d | sed 's/35.237.47.11//g' | sudo tee ip_list
 cat ip_list
 
-#updating the ansible hosts file
+echo '######################updating the ansible hosts file#####################'
 cat <<EOF > /home/reddisekhara_n/iaas-kubernetes/final_list
 [master]
 `sed -n '1p' ip_list`
